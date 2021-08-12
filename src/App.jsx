18e581c5c9a313e3009548dd648e761ec27207bc
iiiGYGYiii <realGYGY@gmail.com>
2021-08-11 22:01:49 -0500

    Add main files of the boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () =>{
+  return(<div className="App">
+    <h1>Hello from React!</h1>
+    <h3>Start editing your project!</h3>
+    <h3>GLHF!</h3>
+  </div>);
+}
+
+export default App;
